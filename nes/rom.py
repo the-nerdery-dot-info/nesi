f4a8a114a9f620ec0ca828735337769dff40a669
Corey Prophitt <prophitt.corey@gmail.com>
2015-09-21 00:29:52 -0700

    Added nes module and basic NesRom class

---
+++ nes/rom.py
@@ -0,0 +1,30 @@
+'''
+    TODO
+'''
+
+# Import standard library modules
+from __future__ import print_function
+
+
+class NesRom(object):
+    '''
+        TODO
+    '''
+
+    def __init__(self, rom):
+        self.rom = rom
+
+    def analyze(self):
+        '''
+            TODO
+        '''
+
+        # Analyze stuff
+
+        return self
+
+    def print_analysis(self):
+        '''
+            TODO
+        '''
+        print('Was given {rom}'.format(rom=self.rom))
