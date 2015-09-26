ee95b92ae83c493387ce54fef900e8cd213c85f0
Corey Prophitt <prophitt.corey@gmail.com>
2015-09-26 00:48:31 -0700

    Added docstrings to mappers.py

--- nes/mappers.py
+++ nes/mappers.py
@@ -1,5 +1,10 @@
 '''
-    TODO
+    The mappers package contains all known information about each NES mapper.
+    The data here was dug up through various websites. For the sake of history,
+    the following sites/docs proved useful:
+
+        1. http://wiki.nesdev.com
+        2. http://tuxnes.sourceforge.net/nesmapper.txt
 '''
 
 NES_MAPPERS = {
@@ -12,8 +17,23 @@ NES_MAPPERS = {
 
 def find(mapper_id):
     '''
-        TODO
+        Takes a mapper ID as an integer and returns an object representing
+        what we know about the mapper. The object looks like:
+
+        {
+            'name': 'Foobar,
+            'examples': 'Foo, Bar, Baz'
+        }
+
+        If we don't have any information on the mapper, a special object is
+        returned,
+
+        {
+            'name': 'Unknown',
+            'examples': 'None'
+        }
     '''
+
     try:
         return NES_MAPPERS[mapper_id]
     except KeyError:
