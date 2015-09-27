7a14544e64fc71bab38939d83625f8895fb66629
Corey Prophitt <prophitt.corey@gmail.com>
2015-09-27 01:44:37 -0700

    Added the list of common mappers from nesdev
    See http://wiki.nesdev.com/w/index.php/List_of_mappers.

--- nes/mappers.py
+++ nes/mappers.py
@@ -108,6 +108,118 @@ NES_MAPPERS = {
         'name': 'VRC6b',
         'examples': 'Unknown',
     },
+    34: {
+        'name': 'BNROM, NINA-001',
+        'examples': 'Unknown',
+    },
+    64: {
+        'name': 'RAMBO-1',
+        'examples': 'Unknown',
+    },
+    66: {
+        'name': 'GxROM, MxROM',
+        'examples': 'Unknown',
+    },
+    68: {
+        'name': 'After Burner',
+        'examples': 'Unknown',
+    },
+    69: {
+        'name': 'FME-7, Sunsoft 5B',
+        'examples': 'Unknown',
+    },
+    71: {
+        'name': 'Camerica/Codemasters',
+        'examples': 'Unknown',
+    },
+    73: {
+        'name': 'VRC3',
+        'examples': 'Unknown',
+    },
+    74: {
+        'name': 'Pirate MMC3 derivative',
+        'examples': 'Unknown',
+    },
+    75: {
+        'name': 'VRC1',
+        'examples': 'Unknown',
+    },
+    76: {
+        'name': 'Namco 109 variant',
+        'examples': 'Unknown',
+    },
+    79: {
+        'name': 'NINA-03/NINA-06',
+        'examples': 'Unknown',
+    },
+    85: {
+        'name': 'VRC7',
+        'examples': 'Unknown',
+    },
+    86: {
+        'name': 'JALECO-JF-13',
+        'examples': 'Unknown',
+    },
+    94: {
+        'name': 'Senjou no Ookami',
+        'examples': 'Unknown',
+    },
+    105: {
+        'name': 'NES-EVENT',
+        'examples': 'Unknown',
+    },
+    113: {
+        'name': 'NINA-03/NINA-06??',
+        'examples': 'Unknown',
+    },
+    118: {
+        'name': 'TxSROM, MMC3 Variant',
+        'examples': 'Unknown',
+    },
+    119: {
+        'name': 'TQROM, MMC3 Variant',
+        'examples': 'Unknown',
+    },
+    159: {
+        'name': 'Bandai EPROM (24C01)',
+        'examples': 'Unknown',
+    },
+    166: {
+        'name': 'SUBOR',
+        'examples': 'Unknown',
+    },
+    167: {
+        'name': 'SUBOR',
+        'examples': 'Unknown',
+    },
+    180: {
+        'name': 'Crazy Climber',
+        'examples': 'Unknown',
+    },
+    185: {
+        'name': 'CNROM with protection diodes',
+        'examples': 'Unknown',
+    },
+    192: {
+        'name': 'Pirate MMC3 derivative',
+        'examples': 'Unknown',
+    },
+    206: {
+        'name': 'DxROM, Namco 118 / MIMIC-1',
+        'examples': 'Unknown',
+    },
+    210: {
+        'name': 'Namco 175 and 340',
+        'examples': 'Unknown',
+    },
+    228: {
+        'name': 'Action 52',
+        'examples': 'Unknown',
+    },
+    232: {
+        'name': 'Camerica/Codemasters Quattro',
+        'examples': 'Unknown',
+    },
 }
 
 
